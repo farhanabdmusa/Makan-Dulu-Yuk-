c8359c0f337ae2393b48ef4f990f9d849e0ffb66
Fachri Izzudin Lazuardi <16.9109@stis.ac.id>
2018-12-12 23:40:10 +0700

    toast data register dan intent ke activity register

--- app/src/main/java/com/robot/mr/makanduluyuk/LoginActivity.java
+++ app/src/main/java/com/robot/mr/makanduluyuk/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.robot.mr.makanduluyuk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText username, password;
+    private Button regButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +22,25 @@ public class LoginActivity extends AppCompatActivity {
         username= findViewById(R.id.username);
         password = findViewById(R.id.password);
 
+        regButton = findViewById(R.id.registerButton);
 
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrationAvtivity();
+            }
+        });
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String value = extras.getString("dataUser");
+            //The key argument here must match that used in the other activity
+            Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void registrationAvtivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
