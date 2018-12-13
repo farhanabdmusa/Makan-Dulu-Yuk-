3fd8625a09a5fa8a68e2aa2b540813e999f8f4c2
Farhan Abdurrahman Musa <16.9123@stis.ac.id>
2018-12-13 16:26:25 +0700

    ubah config url

--- app/src/main/java/com/robot/mr/makanduluyuk/Config.java
+++ app/src/main/java/com/robot/mr/makanduluyuk/Config.java
@@ -2,7 +2,7 @@ package com.robot.mr.makanduluyuk;
 
 public class Config {
 
-    public static final String LOGIN_URL = "https://1cb84600.ngrok.io/mdy/users/login";
+    public static final String LOGIN_URL = "https://a0ab988c.ngrok.io/mdy/users/login";
 
     public static final String JSON_RESPONSE = "response";
 
